058963d0255d0488db31b623b60f658a2150fa4f
John Lee <drumwolf@gmail.com>
2019-10-26 17:17:17 -0700

    simply added comments to organize JS blocks

--- json_diff.js
+++ json_diff.js
@@ -1,8 +1,6 @@
 const left = { 'a':1, 'b':2, 'c':3 };
 const right = { 'a':1, 'b':2, 'c': { 'c1': 1} };
-
 getDiff(left, right);
-
 
 function getKeys(left, right) {
   const keys = [...Object.keys(left), ...Object.keys(right)];
@@ -21,22 +19,31 @@ function getKeys(left, right) {
 
 function getDiff(left, right, parentKey) {
   
+  /* get all keys in left and right objects */
   const allKeys = getKeys(left, right);
 
+  /* loop through all left and right keys */
   for (let i = 0; i < allKeys.length; i++) {
     const key = allKeys[i];
+
+    // when left key and right key are different, start comparison
     if (left[key] !== right[key]) {
+
+      // output subtraction from left key
       if (left[key]) {
         console.log(`-${parentKey}.${key}:${left[key]}`);
       }
-      if (right[key]) {
 
+      // output addition to right key
+      if (right[key]) {
         if (typeof right[key] !== 'object') {
            console.log(`+${parentKey}.${key}:${right[key]}`);
         } else {
           getDiff(left[key], right[key], key);
         }
       }
+
     }    
+    // end of block dealing with left and right key difference
   }
 }
